ef09f64a4c910969b80bc20871cd306a0ed53b7a
Nick <nijaru7@gmail.com>
2026-08-27 19:25:45 -0700

    Format decoder module order

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -12,9 +12,9 @@ mod compressed_trees;
 mod context;
 mod context_map;
 mod decoder;
+mod dictionary;
 #[cfg(test)]
 mod differential_tests;
-mod dictionary;
 mod distance;
 mod history;
 mod metablock_header;
