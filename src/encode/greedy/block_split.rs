600510231fb7801c5652bfb306c44dbe653cd9fc
Nick <nijaru7@gmail.com>
2026-08-30 17:51:13 -0700

    perf: cache current block type

--- src/encode/greedy/block_split.rs
+++ src/encode/greedy/block_split.rs
@@ -44,6 +44,7 @@ pub(super) struct BlockCursor<'a> {
     encoding: &'a BlockSplitEncoding,
     block_index: usize,
     remaining: usize,
+    current_type: usize,
     type_calculator: BlockTypeCodeCalculator,
 }
 
@@ -142,6 +143,7 @@ impl BlockSplitEncoding {
             encoding: self,
             block_index: 0,
             remaining: self.split.lengths[0],
+            current_type: usize::from(self.split.types[0]),
             type_calculator,
         }
     }
@@ -156,6 +158,7 @@ impl BlockCursor<'_> {
         if self.remaining == 0 {
             self.block_index += 1;
             let block_type = self.encoding.split.types[self.block_index];
+            self.current_type = usize::from(block_type);
             let type_symbol = self.type_calculator.next(block_type);
             self.encoding
                 .type_code
@@ -175,7 +178,7 @@ impl BlockCursor<'_> {
         }
 
         self.remaining -= 1;
-        usize::from(self.encoding.split.types[self.block_index])
+        self.current_type
     }
 }
 
